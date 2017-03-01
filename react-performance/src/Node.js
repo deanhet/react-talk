a4c3448924a703c8422b8ab99dfd034e4eade93e
Dean Hetherington <deanhet@gmail.com>
2017-03-01 09:28:45 +0000

    Refactored speed-up with ternary voodoo magic

--- react-performance/src/Node.js
+++ react-performance/src/Node.js
@@ -5,32 +5,25 @@ export default class Node extends React.Component{
     super(props);
     this.state = {
       counter: Math.floor(Math.random() * (10 - 0)),
-      operator: Math.random() < 0.5 ? 'minus' : 'plus',
-      xpos: Math.floor(Math.random() * (window.innerWidth - 0)),
-      ypos: Math.floor(Math.random() * (window.innerHeight - 0))
+      position: Math.floor(Math.random() * (window.innerWidth - 0)),
+      ypos: Math.floor(Math.random() * (window.innerHeight - 0)),
+      operator: Math.random() < 0.5 ? true : false
     }
+    setInterval(() => {
+      const { position, toRight} = this.state,
+            newDirection = (position === (toRight ? window.innerWidth - 50 : 0)) ?
+              !toRight :
+              toRight,
+            newPosition = newDirection ? position + 1 : position - 1;
+
+            this.setState({
+              position: newPosition,
+              toRight: newDirection
+            });
+    }, 30);
   }
   componentDidMount(){
     this.count = setInterval(this.count.bind(this), 1000);
-    this.transition = setInterval(this.transition.bind(this), 10);
-    this.direction = 'left';
-  }
-  transition(){
-    if(this.direction === 'left'){
-      if(this.state.xpos == 0){
-        this.direction = 'right';
-      } else {
-        this.setState({ xpos: this.state.xpos -= 1 });
-      }
-    } else {
-      // right
-      if(this.state.xpos == window.innerWidth - 50){
-        this.direction = 'left';
-      } else {
-        this.setState({ xpos: this.state.xpos += 1 });
-      }
-    }
-
   }
   count(){
     if(this.state.counter === 10){
@@ -51,7 +44,7 @@ export default class Node extends React.Component{
   }
   render(){
     return(
-      <div ref="node" style={{ position: 'absolute', top: this.state.ypos, left: this.state.xpos, background: 'blue', borderRadius: '40px', width: '50px', height: '50px', color: 'white', lineHeight: '1'}}>
+      <div ref="node" style={{ position: 'absolute', top: this.state.ypos, left: this.state.position, background: 'blue', borderRadius: '40px', width: '50px', height: '50px', color: 'white', lineHeight: '1'}}>
         <div style={{ lineHeight: '3em' }}>{this.state.counter}</div>
       </div>
     )
